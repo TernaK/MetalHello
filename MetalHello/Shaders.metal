f851a3cffe49b115b2f068618048cc188d6b465d
Terna Kpamber <tkpamber@yahoo.com>
2017-06-30 02:05:30 -0700

    configure pipeline state

---
+++ MetalHello/Shaders.metal
@@ -0,0 +1,20 @@
+//
+//  Shaders.metal
+//  MetalHello
+//
+//  Created by Terna Kpamber on 6/30/17.
+//  Copyright © 2017 Terna Kpamber. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
+vertex float4 vertex_shader(const device packed_float3* vertices [[buffer(0)]],
+                            unsigned int v_id [[vertex_id]]) {
+  return float4(vertices[v_id], 1.0);
+}
+
+fragment float3 fragment_shader() {
+  return float3(0.4, 0.6, 0.1);
+}
--- MetalHello/ViewController.swift
+++ MetalHello/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import MetalKit
 
 class ViewController: UIViewController {
+  
+  var metal_device: MTLDevice!
+  var metal_layer: CAMetalLayer!
+  var pipeline_state: MTLRenderPipelineState!
+  
+  var vertex_buffer: MTLBuffer!
+  let vertex_data:[Float] = [
+    -1,-1,0, 1,-1,0, 0,1,0
+  ]
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    metal_device = MTLCreateSystemDefaultDevice()
+    metal_layer = CAMetalLayer()
+    metal_layer.device = metal_device
+    metal_layer.framebufferOnly = true
+    metal_layer.pixelFormat = .bgra8Unorm
+    metal_layer.frame = view.layer.frame
+    view.layer.addSublayer(metal_layer)
+    
+    // vertex data
+    let buffer_length = vertex_data.count * MemoryLayout.size(ofValue: vertex_data[0])
+    vertex_buffer = metal_device.makeBuffer(bytes: vertex_data, length: buffer_length, options: [])
+    
+    // shaders
+    let metal_library = metal_device.makeDefaultLibrary()
+    let vertex_shader = metal_library?.makeFunction(name: "vertex_shader")
+    let fragment_shader = metal_library?.makeFunction(name: "fragment_shader")
+    
+    let pipeline_state_descriptor = MTLRenderPipelineDescriptor()
+    pipeline_state_descriptor.vertexFunction = vertex_shader
+    pipeline_state_descriptor.fragmentFunction = fragment_shader
+    pipeline_state_descriptor.colorAttachments[0].pixelFormat = metal_layer.pixelFormat
+    
+    pipeline_state = try! metal_device.makeRenderPipelineState(descriptor: pipeline_state_descriptor)
   }
 
   override func didReceiveMemoryWarning() {
